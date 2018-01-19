00e2cc029ed889281021f0058bbb8e7da9311569
Alexey <alexey.samoylov@gmail.com>
2018-01-20 01:59:43 +0700

    always overwrite the config file on start

--- alpine/0.15.1-r1/docker-entrypoint.sh
+++ alpine/0.15.1-r1/docker-entrypoint.sh
@@ -5,16 +5,14 @@ if [ "$1" = 'bitcoind' ]; then
         echo "Please configure a docker volume at $BITCOIN_DATA"
         exit 1
     fi
-    if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
-		testnet=${BITCOIN_TESTNET:-0}
-		regtest=${BITCOIN_REGTEST:-0}
-		rpcport=${BITCOIN_RPC_PORT:-8332}
-		rpcuser=${BITCOIN_RPC_USER:-rpcuser}
-		rpcpassword=${BITCOIN_RPC_PASSWORD:-rpcpass}
-		rpcallowip=${BITCOIN_RPC_ALLOW_IP:-::/0}
-		EOF
-	fi
+    cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+    testnet=${BITCOIN_TESTNET:-0}
+    regtest=${BITCOIN_REGTEST:-0}
+    rpcport=${BITCOIN_RPC_PORT:-8332}
+    rpcuser=${BITCOIN_RPC_USER:-rpcuser}
+    rpcpassword=${BITCOIN_RPC_PASSWORD:-rpcpass}
+    rpcallowip=${BITCOIN_RPC_ALLOW_IP:-::/0}
+EOF
 	exec bitcoind -server -printtoconsole -datadir="${BITCOIN_DATA}"
 fi
 exec "$@"
